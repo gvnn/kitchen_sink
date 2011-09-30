5c289ff5f9d74eaa48a27a51f577bbf82a951857
gvnn <gvnnrulez@gmail.com>
2011-09-30 15:15:50 +1000

    added desktop notification

---
+++ chrome/js/sync.js
@@ -0,0 +1,21 @@
+var sync = {
+
+  setup: function(dropbox_obj) {
+	this._dropbox = dropbox_obj;
+  },
+  
+  go: function() {
+	this._notification = webkitNotifications.createNotification(
+		'gui/img/icon.png',
+		'kitchan_sync',
+		'Sync started'
+	);
+	this._notification.show();
+	setTimeout("sync.close_notification()", 3000);
+  },
+  
+  close_notification : function() {
+	this._notification.cancel();
+  }
+
+}
