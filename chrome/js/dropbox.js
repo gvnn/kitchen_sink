5ce686aa4cd2ed3bd8373679cf58cac5890ece7e
gvnn <gvnnrulez@gmail.com>
2011-10-03 11:02:37 +1100

    changed the original dropbox js a little bit, moved from jsonp to json datatype. Started to write the file, still getting QUOTA_EXCEEDED_ERR error though

--- chrome/js/dropbox.js
+++ chrome/js/dropbox.js
@@ -1,116 +1,128 @@
 var dropbox = {
-  setup: function(consumerKey, consumerSecret) {
-    this._consumerKey = consumerKey;
-    this._consumerSecret = consumerSecret;
-    this._requestCounter = $.now();
-  },
+
+	setup: function(consumerKey, consumerSecret) {
+		this._consumerKey = consumerKey;
+		this._consumerSecret = consumerSecret;
+		this._requestCounter = $.now();
+	},
   
-  accessToken : function() {
-	return this._accessToken;
-  },
+	accessToken : function() {
+		return this._accessToken;
+	},
   
-  accessTokenSecret : function() {
-	return this._accessTokenSecret;
-  },
+	accessTokenSecret : function() {
+		return this._accessTokenSecret;
+	},
   
-  set_accessTokenSecret : function(value) {
-	this._accessTokenSecret = value;
-  },
+	set_accessTokenSecret : function(value) {
+		this._accessTokenSecret = value;
+	},
   
-  set_accessToken : function(value) {
-	this._accessToken = value;
-  },
+	set_accessToken : function(value) {
+		this._accessToken = value;
+	},
 
-  authenticate: function(email, password, callback) {
-    var that = this;
+	authenticate: function(email, password, callback) {
+		var that = this;
+		this._request("/token", {
+				method : "GET",
+				sendAuth: false,
+				success: function(data) {
+					console.log("authentication response", data);
+					that._accessToken = data.token;
+					that._accessTokenSecret = data.secret;
+					if (callback) {
+						callback();
+					}
+				},
+				error: function() {
+					console.error("authentication error", arguments);
+				}
+			}, {
+				email: email,
+				password: password
+			}
+		);
+	},
 
-    this._request("/token", {
-      sendAuth: false,
+	getInfo: function() {
+		this._request("/account/info", {
+				method : "GET",
+				success: function(data) {
+					console.log("account info", data);
+				},
+				error: function() {
+					console.log("account info error", arguments);
+				}
+			}
+		);
+	},
 
-      success: function(data) {
-        console.log("authentication response", data);
-        that._accessToken = data.token;
-        that._accessTokenSecret = data.secret;
+	sendFile : function(file_name, file_path) {
+		this._request("/files/dropbox/" + file_name, {
+				method : "POST",
+				success: function(data) {
+					console.log("account info", data);
+				},
+				error: function() {
+					console.log("account info error", arguments);
+				}
+			}, {
+				file : file_path 
+			}
+		);
+	},
 
-        if (callback) {
-          callback();
-        }
-      },
-      error: function() {
-        console.error("authentication error", arguments);
-      }
-    }, {
-      email: email,
-      password: password
-    });
-  },
+	_request: function(path, params, data) {
+		params = $.extend({}, {
+			subdomain: "api",
+			apiVersion: "0",
+			sendAuth: true,
+			success: $.noop,
+			error: $.noop
+		}, params || {});
 
-  getInfo: function() {
-    this._request("/account/info", {
-      success: function(data) {
-        console.log("account info", data);
-      },
+		if (params.sendAuth && !this._accessToken) {
+			throw "Authenticated method called before authenticating";
+		}
 
-      error: function() {
-        console.log("account info error", arguments);
-      }
-    });
-  },
+		var url = "https://" + params.subdomain + ".dropbox.com/" + params.apiVersion + path;
 
-  _request: function(path, params, data) {
-    var requestId = "dropboxjsonp" + (this._requestCounter++);
-    params = $.extend({}, {
-      subdomain: "api", // some methods need api-content.dropbox.com
-      apiVersion: "0",
-      sendAuth: true,
+		var message = {
+			action: url,
+			method: params.method,
+			parameters: {
+				oauth_consumer_key: this._consumerKey,
+				oauth_signature_method: "HMAC-SHA1"
+			}
+		};
 
-      method: "GET",
-      success: $.noop,
-      error: $.noop
-    }, params || {});
+		if(params.method == "GET") {
+			$.extend(message.parameters, data);
+		}
 
-    if (params.sendAuth && !this._accessToken) {
-      throw "Authenticated method called before authenticating";
-    }
+		if (params.sendAuth) {
+			message.parameters.oauth_token = this._accessToken;
+		}
 
-    var url = "https://" + params.subdomain + ".dropbox.com/" + params.apiVersion + path;
+		var oauthBits = {
+			consumerSecret: this._consumerSecret
+		};
 
-    var message = {
-      action: url,
-      method: params.method,
-      parameters: {
-        oauth_consumer_key: this._consumerKey,
-        oauth_signature_method: "HMAC-SHA1",
-        callback: requestId
-      }
-    };
+		if (params.sendAuth) {
+			oauthBits.tokenSecret = this._accessTokenSecret;
+		}
 
-    $.extend(message.parameters, data);
+		OAuth.setTimestampAndNonce(message);
+		OAuth.SignatureMethod.sign(message, oauthBits);
 
-    if (params.sendAuth) {
-      message.parameters.oauth_token = this._accessToken;
-    }
-
-    var oauthBits = {
-      consumerSecret: this._consumerSecret
-    };
-
-    if (params.sendAuth) {
-      oauthBits.tokenSecret = this._accessTokenSecret;
-    }
-
-    OAuth.setTimestampAndNonce(message);
-    OAuth.SignatureMethod.sign(message, oauthBits);
-
-    $.ajax({
-      dataType: "jsonp",
-      method: params.method,
-      url: url,
-      data: OAuth.getParameterMap(message.parameters),
-      jsonpCallback: requestId,
-
-      success: params.success,
-      error: params.error
-    });
-  }
+		$.ajax({
+			dataType: "json",
+			method: params.method,
+			url: url,
+			data: OAuth.getParameterMap(message.parameters),
+			success: params.success,
+			error: params.error
+		});
+	}
 };
--- chrome/js/sync.js
+++ chrome/js/sync.js
@@ -1,21 +1,113 @@
 var sync = {
-
-  setup: function(dropbox_obj) {
-	this._dropbox = dropbox_obj;
-  },
   
-  go: function() {
-	this._notification = webkitNotifications.createNotification(
-		'gui/img/icon.png',
-		'kitchen_sync',
-		'Sync started'
-	);
-	this._notification.show();
-	setTimeout("sync.close_notification()", 3000);
-  },
+	go: function() {
+		this._notification = webkitNotifications.createNotification(
+			'gui/img/icon.png',
+			'kitchen_sync',
+			'Sync started'
+		);
+		this._notification.show();
+		setTimeout("sync.close_notification()", 3000);
+		
+		//collecting bookmarks
+		this._load_local_bookmarks();
+	},
   
-  close_notification : function() {
-	this._notification.cancel();
-  }
+	close_notification : function() {
+		this._notification.cancel();
+	}, 
+  
+	_load_local_bookmarks : function() {
+		chrome.bookmarks.getTree(function(results) {
+			console.log("bookmarks loaded: " + results.length);
+			//save bookmarks
+			sync.local_bookmarks = results;
+			//new filesystem, i need that to build the files to send to dropbox
+			window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+			window.requestFileSystem(window.PERSISTENT, 1024*1024, sync._on_init_fs, sync._fs_error_handler); // 1 MB should be enough
+		});
+	},
+	
+	_on_init_fs : function(fs) {
+		console.log('Opened file system: ' + fs.name);
+		console.log(JSON.stringify(localStorage).length);
+		//write file
+		/*fs.root.getFile('bookmarks.json', { create: true }, function(fileEntry) {
+		    sync.bookmarks_full_path = fileEntry.fullPath;
+			fileEntry.createWriter(function(fileWriter) {
+				window.BlobBuilder  = window.BlobBuilder || window.WebKitBlobBuilder;
+				var bb = new window.BlobBuilder();
+				bb.append(JSON.stringify(sync.local_bookmarks));
+				fileWriter.write(bb.getBlob('text/plain'));
+				//send file to dropbox
+				dropbox.getInfo();
+        dropbox.sendFile("bookmarks.json", window.webkitURL.createObjectURL(bb));
+			}, sync._fs_error_handler);
+		}, sync._fs_error_handler);*/
+		  
+		  filename = 'bookmarks.json';
+		  data = JSON.stringify(sync.local_bookmarks);
+		  console.log(data)
+		  
+		  if (!window.BlobBuilder && window.WebKitBlobBuilder) {
+            window.BlobBuilder = window.WebKitBlobBuilder;
+          }
 
+          fs.root.getFile(filename, {create: true}, function(fileEntry) {
+
+          // Create a FileWriter object for our FileEntry (log.txt).
+          fileEntry.createWriter(function(fileWriter) {
+
+            fileWriter.onwriteend = function(e) {
+              console.log('Write completed.');
+            };
+
+            fileWriter.onerror = function(e) {
+              console.log('Write failed: ' + e.toString());
+            };
+
+           var builder = new BlobBuilder();
+           builder.append(data);
+           var blob = builder.getBlob();
+           fileWriter.write(blob);
+           
+           var url = window.webkitURL.createObjectURL(blob);
+           console.log(url);
+
+          }, sync._fs_error_handler);
+
+          }, sync._fs_error_handler);
+		  
+		  /*
+      var builder = new BlobBuilder();
+      builder.append();
+      var blob = builder.getBlob();
+      var url = window.webkitURL.createObjectURL(blob);
+      console.log(url);*/
+	},
+	
+	_fs_error_handler : function(e) {
+		var msg = '';
+		switch (e.code) {
+			case FileError.QUOTA_EXCEEDED_ERR:
+				msg = 'QUOTA_EXCEEDED_ERR';
+				break;
+			case FileError.NOT_FOUND_ERR:
+				msg = 'NOT_FOUND_ERR';
+				break;
+			case FileError.SECURITY_ERR:
+				msg = 'SECURITY_ERR';
+				break;
+			case FileError.INVALID_MODIFICATION_ERR:
+				msg = 'INVALID_MODIFICATION_ERR';
+				break;
+			case FileError.INVALID_STATE_ERR:
+				msg = 'INVALID_STATE_ERR';
+				break;
+			default:
+				msg = 'Unknown Error';
+				break;
+		};
+		console.error('Error: ' + msg);
+	}
 }
